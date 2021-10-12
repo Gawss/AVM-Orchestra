1bf39aa3e37803e1e41839414fd78b2d927144b6
Luis M. Palacio <luispalacio1997@hotmail.com>
2021-10-12 13:44:34 +0200

    calimero done

--- public/Calimero/sketch.js
+++ public/Calimero/sketch.js
@@ -1,101 +1,117 @@
-var diam = 50;
-var colorFondo = 128;
-var colorHuevo = 255;
-
-var anchoHuevo = 75;
-var altoCorteHuevo = 40;
-var diametroCabeza = 60;
-var radioCabeza = diametroCabeza/2;
 
 let img;
-let drawGuide = false;
+let calimeros = [];
 
-//    create_canvas
-// -------- funcion SETUP ----------
 function setup() {
     createCanvas(windowWidth, windowHeight);
     
     img = loadImage('./Resources/Images/calimero.png');
     angleMode(DEGREES);
+    textAlign(CENTER, CENTER);
 }
-// -------- funcion DRAW ----------
-function draw() {
-    // background(100);
 
-    drawCalimero(width/2, height/2);
-    if(drawGuide) drawGuideImg();
+
+function draw() {
+    background(100);
+    
+    drawGuideImg();
+
+    calimeros.forEach(calimero => {
+        calimero.draw(mouseX, mouseY);
+    });
+    
 }
 
 function drawGuideImg(){
     push();
     tint(100, 127);
-    image(img, (width/2) - (img.width/2), (height/2) - (img.height/2));
+    image(img, 0, 0);
+
+    fill(255);
+    textSize(windowWidth*0.05);
+    noStroke();
+    text("Click somewhere!", windowWidth/2, windowHeight - windowHeight*0.1);
+
+    textSize(windowWidth*0.02)
+    text("x" + calimeros.length, img.width/2, img.height - img.height * 0.1);
     pop();
 }
 
-// -------- funcion MIDIBUJO ----------
-function drawCalimero(posX,  posY) {
-
-    // head
-    fill(0);
-    noStroke();
-    circle(posX, posY-(img.height*0.106), 68);
-
-    // main body
-    noStroke();
-    fill(0);
-    circle(posX, posY, 45, 90);
-
-    // eyes
-
-    fill(255);
-    noStroke();
-    circle(posX-(img.height*0.02), posY-(img.height*0.095), 22); // Left eye
-    circle(posX+(img.height*0.025), posY-(img.height*0.095), 22); // Right eye
-
-    fill(120, 30, 250);
-    circle(posX-(img.height*0.02), posY-(img.height*0.095), 10); // left pupil
-    circle(posX+(img.height*0.025), posY-(img.height*0.095), 10); // right pupil
-
-    fill(255);
-    circle(posX-(img.height*0.027), posY-(img.height*0.098), 5); // leftBright
-    circle(posX+(img.height*0.0185), posY-(img.height*0.098), 5); // leftBright
-
-    // nose
-    stroke(180, 90, 22);
-    strokeWeight(3);
-    noFill();
-    arc(posX, posY-(img.height*0.052), 20, 5, 180, 360);
-
-    // hands
-    var handLongitude = 3;
-    stroke(0);
-    strokeWeight(15);
-    strokeCap(ROUND);
-
-    line(posX-(img.width*0.03), posY - (img.height*0.016), posX-(img.width*0.1)-handLongitude, posY+(img.height*0.01)); // left hand
-    line(posX+(img.width*0.03), posY - (img.height*0.016), posX+(img.width*0.1)+handLongitude, posY+(img.height*0.01)); // right hand
-
-    // legs
-    var legLongitude = 35;
-
-    strokeCap(ROUND);
-    line(posX-(img.width*0.02), posY + (img.height*0.01), posX-(img.width*0.02), posY+(img.height*0.01)+ legLongitude); // left leg
-    line(posX+(img.width*0.02), posY + (img.height*0.01), posX+(img.width*0.02), posY+(img.height*0.01) + legLongitude); // right leg
-
-    // egg-cap
-    stroke(0);
-    strokeWeight(2);
-    fill(255);
-    arc(posX, posY-(img.height*0.114), 80, 128, 180, 360, PIE);
-
-}
-
 function touchStarted() {
-    drawGuide = !drawGuide;
-    drawCalimero(mouseX, mouseY);
+    calimeros.push(new Calimero(mouseX, mouseY));
 }
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
+}
+
+class Calimero {
+    constructor(posX, posY) {
+      this.positionX = posX;
+      this.positionY = posY;
+
+      this.eyesOffset = {left:{x:0, y:0}, right:{x:0, y:0}};
+    }
+
+    draw(eyesTargetX, eyesTargetY, headColor){
+
+        // head
+        fill(0);
+        noStroke();
+        circle(this.positionX, this.positionY-(img.height*0.106), 68);
+
+        // main body
+        noStroke();
+        fill(0);
+        circle(this.positionX, this.positionY, 45, 90);
+
+        // eyes
+
+        this.eyesOffset.left.x = max(-4, min(eyesTargetX-this.positionX-(img.height*0.02), 4));
+        this.eyesOffset.left.y = max(-4, min(eyesTargetY-this.positionY-(img.height*0.095), 4));
+
+        this.eyesOffset.right.x = max(-4, min(eyesTargetX-this.positionX+(img.height*0.0185), 4));
+        this.eyesOffset.right.y = max(-4, min(eyesTargetY-this.positionY-(img.height*0.098), 4));
+
+        fill(255);
+        noStroke();
+        circle(this.positionX-(img.height*0.02), this.positionY-(img.height*0.095), 22); // Left eye
+        circle(this.positionX+(img.height*0.025), this.positionY-(img.height*0.095), 22); // Right eye
+
+        fill(120, 30, 250);
+        circle(this.positionX-(img.height*0.02) + this.eyesOffset.left.x, this.positionY-(img.height*0.095) + this.eyesOffset.left.y, 10); // left pupil
+        circle(this.positionX+(img.height*0.025) + this.eyesOffset.right.x, this.positionY-(img.height*0.095) + this.eyesOffset.right.y, 10); // right pupil
+
+        fill(255);
+        circle(this.positionX-(img.height*0.027) + this.eyesOffset.left.x, this.positionY-(img.height*0.098) + this.eyesOffset.left.y, 5); // leftBright
+        circle(this.positionX+(img.height*0.0185) + this.eyesOffset.right.x, this.positionY-(img.height*0.098) + this.eyesOffset.right.y, 5); // leftBright
+
+        // nose
+        stroke(180, 90, 22);
+        strokeWeight(3);
+        noFill();
+        arc(this.positionX, this.positionY-(img.height*0.052), 20, 5, 180, 360);
+
+        // hands
+        var handLongitude = 3;
+        stroke(0);
+        strokeWeight(15);
+        strokeCap(ROUND);
+
+        line(this.positionX-(img.width*0.03), this.positionY - (img.height*0.016), this.positionX-(img.width*0.1)-handLongitude, this.positionY+(img.height*0.01)); // left hand
+        line(this.positionX+(img.width*0.03), this.positionY - (img.height*0.016), this.positionX+(img.width*0.1)+handLongitude, this.positionY+(img.height*0.01)); // right hand
+
+        // legs
+        var legLongitude = 35;
+
+        strokeCap(ROUND);
+        line(this.positionX-(img.width*0.02), this.positionY + (img.height*0.01), this.positionX-(img.width*0.02), this.positionY+(img.height*0.01)+ legLongitude); // left leg
+        line(this.positionX+(img.width*0.02), this.positionY + (img.height*0.01), this.positionX+(img.width*0.02), this.positionY+(img.height*0.01) + legLongitude); // right leg
+
+        // egg-cap
+        stroke(0);
+        strokeWeight(2);
+        fill(255);
+        arc(this.positionX, this.positionY-(img.height*0.114), 80, 128, 180, 360, PIE);
+    }
 }
